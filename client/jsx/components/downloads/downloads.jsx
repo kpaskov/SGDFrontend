96eb9f07ad196441a4f0ad6038396d3eb23157fc
felixgondwe <zungwala@gmail.com>
2017-04-12 15:58:04 -0700

    misc changes

---
+++ client/jsx/components/downloads/downloads.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
+//import SortableTree from 'react-sortable-tree';
+
+export default class Downloads extends Component{
+    /*constructor(props) {
+        super(props);
+
+        this.state = {
+            treeData: [{ title: 'Chicken', children: [ { title: 'Egg' } ] }],
+        };
+    }
+
+    render() {
+        return (
+            <div style={{ height: 400 }}>
+                <SortableTree
+                    treeData={this.state.treeData}
+                    onChange={treeData => this.setState({ treeData })}
+                />
+            </div>
+        );
+    }*/
+    render(){
+        return(<div>Hello</div>);
+    }
+}
