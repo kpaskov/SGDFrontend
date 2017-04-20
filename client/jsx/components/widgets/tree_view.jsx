5a59bd3b8a118e110b8b908195967480a0c40a07
felixgondwe <gondwefelix8@gmail.com>
2017-04-20 16:39:02 -0700

    css module for tree plugin

--- client/jsx/components/widgets/tree_view.jsx
+++ client/jsx/components/widgets/tree_view.jsx
@@ -4,37 +4,43 @@
  * purpose: widget to render tree-view
  */
 import React,{Component} from "react";
-import ClassNames from "classnames"
+import ClassNames from "classnames";
+import cssStyles from '../downloads/downloads_styles.css';
+
 export default class CustomTreeView extends Component {
     constructor(props){
         super(props);
         this.state ={
             visible:true
         };
+       
     };
     toggle(){
+      
         this.setState({visible:!this.state.visible});
     };
     render(){
         let childNodes;
         let style;
-        const classObj = {
+        let classObj;
+
+        if(this.props.node.childNodes!= undefined){
+            childNodes = this.props.node.childNodes.map((node, index)=>{
+                return <li key={index}><CustomTreeView node={node} /> </li>
+            });
+            classObj = {
             'togglable':true,
             'togglable-down': this.state.visible,
             'togglable-up':!this.state.visible
         };
-
-        if(this.props.node.childNodes != null){
-            childNodes = this.props.node.childNodes.map((node, index)=>{
-                return <li><CustomTreeView node={node} /> </li>
-            });
         }
-        if(!this.state.visible){
+        if(this.state.visible){
             style = {display:'none'};
         }
+      
         return(
             <div>
-                <h5 onClick={this.toggle} className={ClassNames(classObj)}>
+                <h5 onClick={this.toggle.bind(this)} className={ClassNames(classObj)}>
                     {this.props.node.title}
                 </h5>
                 <ul style={style}>
