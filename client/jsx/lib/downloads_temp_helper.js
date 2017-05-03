38e1392fa45b5bad90122dd1fe92a1a23339fa50
felixgondwe <gondwefelix8@gmail.com>
2017-05-03 10:32:21 -0700

    testing git repo data

--- client/jsx/lib/downloads_temp_helper.js
+++ client/jsx/lib/downloads_temp_helper.js
@@ -4,8 +4,8 @@
  * 
  */
 import axios from 'axios';
-const D_API_KEY = '';
-const D_ROOT_URL = '';
+const D_API_KEY = 'c3064594b36b486730fc41c5d473c532de444cd7';
+const D_ROOT_URL = `https://api.github.com/?access_token=${D_API_KEY}`;
 
 export function getDownloadsData(){
     const url = '';
