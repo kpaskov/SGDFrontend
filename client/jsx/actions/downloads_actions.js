d0892b000f03a7f95d1104a540610ae4a9d531ca
felixgondwe <gondwefelix8@gmail.com>
2017-05-17 09:39:13 -0700

    added redux setup and css

--- client/jsx/actions/downloads_actions.js
+++ client/jsx/actions/downloads_actions.js
@@ -1,10 +1,37 @@
-import {getDownloadsData} from '../lib/downloads_temp_helper';
+import * as URLS from '../lib/downloads_helper';
+import Axios from 'axios';
+import * as ActionTypes from './actionTypes';
 
-export const FETCH_DOWNLOADS_DATA = 'FETCH_DOWNLOADS_DATA';
-
-export function getData(){
+export const fetchDownloadResultsSuccess = (results) => {
     return {
-        type:FETCH_DOWNLOADS_DATA,
-        payload: getDownloadsData
-    }
-}
+        type: ActionTypes.FETCH_DOWNLOADS_RESULTS_SUCCESS,
+        payload: results
+    };
+};
+
+export const fetchDownloadsMenuSuccess = (results) => {
+    return {
+        type: ActionTypes.FETCH_DOWNLOADS_MENU,
+        payload: results
+    };
+};
+
+export function fetchDownloadsMenuData(){
+      return (dispatch) => {
+        return Axios.get(URLS.menuUrl)
+            .then(response => {
+                dispatch(fetchDownloadsMenuSuccess(response))
+            })
+            .catch(error => { throw (error) });
+    };
+};
+
+export const fetchDownloadResults = (searchTerm) => {
+    return (dispatch) => {
+        return Axios.get(URLS.resultsUrl)
+            .then(response => {
+                debugger
+                dispatch(fetchDownloadResultsSuccess(response.data))
+            }).catch(error => { throw (error) });
+    };
+};
