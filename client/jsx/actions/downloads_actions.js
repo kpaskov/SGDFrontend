dc0227ea8aff8daff2e5bf8ba7bf77cb559e43b5
felixgondwe <gondwefelix8@gmail.com>
2017-05-26 17:05:22 -0700

    added event hadlers for tree toggle and url change

--- client/jsx/actions/downloads_actions.js
+++ client/jsx/actions/downloads_actions.js
@@ -1,21 +1,31 @@
 import * as URLS from '../lib/downloads_helper';
 import Axios from 'axios';
 import * as ActionTypes from './actionTypes';
-
+import _ from 'underscore';
+/**
+ * 
+ * @param {*} results 
+ */
 export const fetchDownloadResultsSuccess = (results) => {
     return {
         type: ActionTypes.FETCH_DOWNLOADS_RESULTS_SUCCESS,
         payload: results
     };
 };
-
+/**
+ * 
+ * @param {*} results 
+ */
 export const fetchDownloadsMenuSuccess = (results) => {
     return {
         type: ActionTypes.FETCH_DOWNLOADS_MENU,
         payload: results
     };
 };
-
+/**
+ * 
+ * @returns 
+ */
 export function fetchDownloadsMenuData(){
       return (dispatch) => {
         return Axios.get(URLS.menuUrl)
@@ -25,7 +35,10 @@ export function fetchDownloadsMenuData(){
             .catch(error => { throw (error) });
     };
 };
-
+/**
+ * 
+ * @param {*} searchTerm 
+ */
 export const fetchDownloadResults = (searchTerm) => {
 
     return (dispatch) => {
@@ -33,6 +46,54 @@ export const fetchDownloadResults = (searchTerm) => {
             .then(response => {
                 dispatch(fetchDownloadResultsSuccess({datasets:response.data,searchTerm:searchTerm}));
             }).catch(error => { throw (error) });
+    };
+};
+/**
+ * Gets the node that user clicked
+ * add the selected node to state
+ * @param {*} key 
+ */
+export const getNode = (key,list) => {
+    return (dispatch) =>{
+        return dispatch(_.findWhere(list,{key:key}));
+    }
+
+};
+/**
+ * 
+ * @param {*} leafKey 
+ * @param {*} list 
+ */
+export const getLeaf = (leafKey,list) => {
+     return (dispatch) =>{
+        return dispatch(_.findWhere(list,{key:leafKey}));
+    }
+};
+/**
+ * 
+ * @param {*} leafKey 
+ * @param {*} list 
+ */
+export const deleteLeaf = (leafKey,list) => {
+    return (dispatch)=>{
+        return dispatch(_.without(list, _.findWhere(list,{key:leafKey})));
+    }
+
+};
+/**
+ * 
+ * @param {*} key 
+ * @param {*} list 
+ */
+export const deleteNode = (key,list) => {
+    return (dispatch)=>{
+        return dispatch(_.without(list, _.findWhere(list,{key:key})));
+    }
+};
+
+export const toggleNode = (flag)=>{
+    return (dispatch) => {
+        return dispatch();
     };
 };
 
--- client/jsx/reducers/downloads_reducer.js
+++ client/jsx/reducers/downloads_reducer.js
@@ -8,28 +8,42 @@ const initialState = {
     downloadsResults: [],
     query: '',
     selectedLeaf: '',
+    selectedNodes: [],
+    openNodes: [],
     url: '',
     queryParams: '',
-    tableColumns: []
+    tableColumns: [],
+    nodeVisible:true
 };
 
 export default function (state = initialState, action) {
     if (action.type === '@@router/UPDATE_LOCATION' && action.payload.pathname === '/downloads') {
-        debugger
-        let params = action.payload.query;
-        console.log('params', params.q);
+        /**
+         * listens to changes in the url
+         * fetch data with new query when it changes
+         */
+
+        state.query = (typeof action.payload.query.q === 'string') ? action.payload.query.q : '';
+        return state;
     }
     switch (action.type) {
         case ActionTypes.FETCH_DOWNLOADS_RESULTS_SUCCESS:
             return Object.assign({}, state, {
-                
                 downloadsResults: action.payload.datasets,
-                query: { q:action.payload.searchTerm},
+                query: { q: action.payload.searchTerm },
                 selectedLeaf: action.payload.searchTerm
             });
         case ActionTypes.FETCH_DOWNLOADS_MENU:
             return Object.assign({}, state, {
                 downloadsMenu: state.downloadsMenu.concat(action.payload.data)
+            });
+        case ActionTypes.GET_SELECTED_NODE:
+            return Object.assign({}, state, {
+                selectedNodes: state.selectedNodes.concat(action.payload.data)
+            });
+         case ActionTypes.GET_SELECTED_LEAF:
+            return Object.assign({}, state, {
+                selectedLeaf: {key:'',leaf:''}
             });
         default:
             return state;
