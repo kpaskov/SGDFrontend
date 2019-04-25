8611077ba673397b778cf1377aa09023c2a2475e
sweng66 <sweng@stanford.edu>
2019-04-25 14:21:12 -0700

    testing

--- src/sgd/frontend/yeastgenome/views/misc_views.py
+++ src/sgd/frontend/yeastgenome/views/misc_views.py
@@ -232,7 +232,7 @@ def new_colleague(request):
 @view_config(route_name='submit_data')
 def submit_data(request):
     ci_base = 'https://curate.qa.yeastgenome.org'
-    return render_to_response(TEMPLATE_ROOT + 'submit_data_iframe.jinja2', { 'ci_url': 'new_colleague', 'ci_base': ci_base }, request=request)
+    return render_to_response(TEMPLATE_ROOT + 'submit_data_iframe.jinja2', { 'ci_url': 'author_response', 'ci_base': ci_base }, request=request)
 
 
 @view_config(route_name='primer3')
