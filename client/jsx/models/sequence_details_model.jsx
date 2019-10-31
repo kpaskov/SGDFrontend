fe4bd2d6c1ce08d534f5f8573cfd7829b4dd9874
sweng66 <sweng@stanford.edu>
2019-10-31 15:34:43 -0700

    testing

--- client/jsx/models/sequence_details_model.jsx
+++ client/jsx/models/sequence_details_model.jsx
@@ -6,7 +6,7 @@ var _ = require("underscore");
 var BaseModel = require("./base_model.jsx");
 var LocusFormatHelper = require("../lib/locus_format_helper.jsx");
 
-MAIN_STRAIN_NAME = "S288C";
+var MAIN_STRAIN_NAME = "S288C";
 
 var main_strain_list = ["S288C", "W303", "Sigma1278b", "SK1", "SEY6210", "X2180-1A", "CEN.PK", "D273-10B", "JK9-3d", "FL100", "Y55", "RM11-1a"];
 
