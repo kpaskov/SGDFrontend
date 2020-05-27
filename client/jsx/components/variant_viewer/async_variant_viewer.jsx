0c12e75da2c1b4bdc8391ad7edc3eadaa9366a47
Shuai Weng <sweng@stanford.edu>
2020-05-27 15:54:13 -0700

    testing

--- client/jsx/components/variant_viewer/async_variant_viewer.jsx
+++ client/jsx/components/variant_viewer/async_variant_viewer.jsx
@@ -193,6 +193,9 @@ var AsyncVariantViewer = createReactClass({
     });
     if (variantData.length === 0) return this._renderEmptyNode();
     var caption = this._getDateStr();
+
+    console.log("upstream_chrom_start="+upstream_chrom_start+ ", upstream_chrom_end="+upstream_chrom_end);
+      
     return (
       <VariantViewerComponent
         name={data.name}
