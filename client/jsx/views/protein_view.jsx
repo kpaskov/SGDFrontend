c1a334723d0b73af68e0bd0476dd6548f8992b6c
Shuai Weng <sweng@stanford.edu>
2020-06-12 10:25:41 -0700

    testing

--- client/jsx/views/protein_view.jsx
+++ client/jsx/views/protein_view.jsx
@@ -2,7 +2,7 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
-const ProteinViewer = require('sgd_visualization').ProteinViewerComponent;
+const ProteinViewer = require('sgd_visualization_beta').ProteinViewerComponent;
 const _ = require('underscore');
 const NavBar = require('../components/widgets/navbar.jsx');
 const TabsModel = require('../models/tabs_model.jsx');
