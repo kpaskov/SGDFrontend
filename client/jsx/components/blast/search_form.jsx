48fb76749c2a82e04df08d85687adb73f770e63a
Shuai Weng <sweng@stanford.edu>
2021-01-13 15:54:22 -0800

    testing

--- client/jsx/components/blast/search_form.jsx
+++ client/jsx/components/blast/search_form.jsx
@@ -414,10 +414,10 @@ BLAST Help at NCBI</a>.</p><hr>';
   _setDefaultDatabase: function (data) {
     var database = data.database;
     var datagroup = data.datagroup;
-    var _databaseDef = data.databasedef;
+    var _databaseDef = data.databasedef4pretein;
     var param = this.state.param;
     if (param['type'] == 'protein') {
-        _databaseDef = data.databasedef4protein;
+      _databaseDef = data.databasedef4protein;
     }
 
     var defaultDatabase = [];
