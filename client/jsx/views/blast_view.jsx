6712eb8c0476f20aab03e1c539bf8ab11934a582
sweng66 <sweng@stanford.edu>
2021-04-28 15:51:01 -0700

    testing

---
+++ client/jsx/views/blast_view.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var SearchForm = require('../components/blast/blast_search_form.jsx');
+
+var blastView = {};
+blastSgdView.render = function () {
+  ReactDOM.render(
+    <SearchForm blastType="sgd" />,
+    document.getElementById('j-main')
+  );
+};
+
+module.exports = blastView;
